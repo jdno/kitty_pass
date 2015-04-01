b5a7f947d0b41ec86fa7dbb9387b37f35936e237
Jan David <jandavid@awesometechnology.de>
2015-04-01 22:28:21 +0200

    Configure test environment
    Tests are written using RSpec, factories and, if necessary, Faker. For feature specs, Capybara can be used.

---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,13 @@
+# Load FactoryGirl's methods, and lint all factories.
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+end
