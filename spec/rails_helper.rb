62b79fe9046ed98f35417cbc24851e81193f8319
Jan David <jandavid@awesometechnology.de>
2015-06-18 21:24:18 +0200

    Clean database between specs
    DatabaseCleaner has been reconfigured to clean the database between every spec. This ensures that uniqueness constraints are complied with all the time.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,9 +17,7 @@ require 'rspec/rails'
 # of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
-#
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-require 'support/factory_girl'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -32,7 +30,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
