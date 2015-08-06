730c2947af8dc6d256717d4f9516d572fda5822b
Jan David <jandavid@awesometechnology.de>
2015-08-06 16:34:31 +0200

    Save associations for servers
    When creating or editing a server resource, its associations are not
    saved. This makes it impossible to assign a status, model or location to
    any server.
    
    This bug is caused by an error in the servers' form: The selection menu
    is built using the 'select_tag' method, and not with 'f.select'. Only
    the latter wraps the selection menu inside the :adonis or :proteus hash,
    which is expected by the controller. Due to this bug, the selection was
    never processed.
    
    To fix the bug, the form has been changed to use 'f.select'.
    Furthermore, specs have been written to verify that the associations are
    saved correctly when creating or editing servers. The view specs have
    been adjusted as well work with the selections' changed identifiers.
    
    Fixes gh-2

--- spec/views/adonis/edit.html.erb_spec.rb
+++ spec/views/adonis/edit.html.erb_spec.rb
@@ -23,15 +23,15 @@ RSpec.describe 'adonis/edit.html.erb', type: :view do
   end
 
   it 'provides a selection to change the model' do
-    expect(rendered).to have_selector 'select#model_id'
+    expect(rendered).to have_selector 'select#adonis_model_id'
   end
 
   it 'provides a selection to change the status' do
-    expect(rendered).to have_selector 'select#status_id'
+    expect(rendered).to have_selector 'select#adonis_status_id'
   end
 
   it 'provides a selection to change the location' do
-    expect(rendered).to have_selector 'select#location_id'
+    expect(rendered).to have_selector 'select#adonis_location_id'
   end
 
   it 'provides a text field to change the IPv4 gateway' do
